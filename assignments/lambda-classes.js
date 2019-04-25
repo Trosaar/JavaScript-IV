9f6ac1478372e8b277528072a9b1f96972a2e456
Aaron T <troskaaaron@gmail.com>
2019-04-24 21:12:11 -0500

    started Lambda Class. Working on Instructor Methods.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,38 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(info) {
+    this.name = info.name;
+    this.age = info.age;
+    this.location = info.location;
+    this.gender = info.gender;
+  }
+
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I am from ${this.location}.` );
+  }
+}
+
+class Instructor extends Person {
+  constructor(info) {
+    super(info);
+    this.specialty = info.specialty;
+    this.favLanguage = info.favLanguage;
+    this.catchPhrase = info.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+}
+
+class Student {
+  constructor() {
+
+  }
+}
+
+class ProjectManager {
+  constructor() {
+
+  }
+}
